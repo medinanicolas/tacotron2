3093b07885d26b1572dd43b7a6236c98a512dedf
Nicolas Medina S <medinanicolas@protonmail.com>
2023-05-05 20:02:43 -0400

    symbols fix

--- text/symbols.py
+++ text/symbols.py
@@ -18,4 +18,4 @@ _letters = 'abcdefghijklmnopqrstuvwxyzáéíóúüñ'
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + #_arpabet
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) #+ _arpabet
